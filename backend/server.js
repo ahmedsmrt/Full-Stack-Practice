e6d11301cb9724c6aa655bf6ff2e94383d4e17ab
ahmedsmrt <ahmedsmrt@gmail.com>
2022-07-26 00:05:36 -0400

    Connecting backend to MongoDB using mongoose and then adding middleware file to handle Errors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require("express")
+const colors = require("colors")
 const dotenv = require('dotenv').config()
-const port = process.env.EXPRESS_PORT
 
 const path = "blogpost"
 
+const {errorHandler} = require("./middleware/errorMiddleware")
+const connectDB = require('./config/db')
+const port = process.env.EXPRESS_PORT
+
+connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.use(`/api/${path}`, require('./routes/blogRoutes'))
+
+app.use(errorHandler)
+
 
 app.listen(port, () => console.log(`Server is started on port ${port}`))
